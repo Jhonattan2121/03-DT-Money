77ed6903b78fdfa47110b969fb6d641f7bf9f60a
Jhonattan <jhonattan.ferreira@dlsoftwarehouse.com>
2023-03-30 06:35:54 -0300

    Tabela de transações

--- src/pages/TransActions/index.tsx
+++ src/pages/TransActions/index.tsx
@@ -1,11 +1,46 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import {
+  PriceHighLight,
+  TransActionContainer,
+  TransActionsTable,
+} from './styles'
 
 export function TransActions() {
   return (
     <div>
       <Header />
       <Summary />
+      <TransActionContainer>
+        <TransActionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHighLight variant="outcome">- R$ 59,00</PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Aluguel do apartamento</td>
+              <td>
+                <PriceHighLight variant="outcome">- R$ 1.200,00</PriceHighLight>
+              </td>
+              <td>Casa</td>
+              <td>27/03/2022</td>
+            </tr>
+          </tbody>
+        </TransActionsTable>
+      </TransActionContainer>
     </div>
   )
 }

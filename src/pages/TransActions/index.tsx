5a506b0c3aa56eb11bab1f0cb21f413345b5635b
Jhonattan <jhonattan.ferreira@dlsoftwarehouse.com>
2023-03-30 07:35:58 -0300

    Componente: SearchForm

--- src/pages/TransActions/index.tsx
+++ src/pages/TransActions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import { SearchForm } from './components/SearchForm'
 import {
   PriceHighLight,
   TransActionContainer,
@@ -12,6 +13,7 @@ export function TransActions() {
       <Header />
       <Summary />
       <TransActionContainer>
+        <SearchForm />
         <TransActionsTable>
           <tbody>
             <tr>

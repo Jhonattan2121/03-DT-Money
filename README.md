16b23ef71e4c6a6a084c103deb558b28fc4635bb
Jhonattan <jhonattan.ferreira@dlsoftwarehouse.com>
2023-03-30 07:56:57 -0300

    Criando um modal acessível

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ pnpm i @types/styled-components
 
 pnpm i phosphor-react
 
+pnpm install @radix-ui/react-dialog
+
 
 
 git push -u origin Conexão com a API : proxima brach
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { HeaderContainer, HeaderContent, NewTransActionButton } from './styles'
+import * as Dialog from '@radix-ui/react-dialog'
 
 import LogoImg from '../../assets/Logo.svg'
 
@@ -8,7 +9,19 @@ export function Header() {
       <HeaderContainer>
         <HeaderContent>
           <img src={LogoImg} alt="" />
-          <NewTransActionButton>Nova Transação</NewTransActionButton>
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <NewTransActionButton>Nova Transação</NewTransActionButton>
+            </Dialog.Trigger>
+
+            <Dialog.Portal>
+              <Dialog.Overlay />
+              <Dialog.Content>
+                <Dialog.Title>Nova transação</Dialog.Title>
+                <Dialog.Close />
+              </Dialog.Content>
+            </Dialog.Portal>
+          </Dialog.Root>
         </HeaderContent>
       </HeaderContainer>
     </div>
